847b4fb5d83417ae2cdde2dc1cb48c0ae2de62a9
Vanessa Henderson <bookvanny@gmail.com>
2019-06-04 17:50:35 -0400

    components and routing set up

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Layout, Header, Navigation, Drawer, Content } from "react-mdl";
+import Main from "./components/main";
+
+function App() {
+  return (
+    <div style={{ height: "300px", position: "relative" }}>
+      <Layout fixedHeader>
+        <Header
+          title={
+            <span>
+              <span style={{ color: "#ddd" }}>Area / </span>
+              <strong>The Title</strong>
+            </span>
+          }
+        >
+          <Navigation>
+            <a href="/">Link</a>
+            <a href="/">Link</a>
+            <a href="/">Link</a>
+            <a href="/">Link</a>
+          </Navigation>
+        </Header>
+        <Drawer title="Title">
+          <Navigation>
+            <a href="/">Link</a>
+            <a href="/">Link</a>
+            <a href="/">Link</a>
+            <a href="/">Link</a>
+          </Navigation>
+        </Drawer>
+        <Content>
+          <div className="page-content" />
+          <Main />
+        </Content>
+      </Layout>
+    </div>
+  );
+}
+
+export default App;
