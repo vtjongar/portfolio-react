9658180cd61589217edbb92af0ce1929e090d877
Vanessa Henderson <bookvanny@gmail.com>
2019-06-10 21:45:47 -0400

    added state to work tabs

--- src/components/work.js
+++ src/components/work.js
@@ -5,12 +5,13 @@ import { Tabs, Tab } from 'react-mdl';
 class Projects extends Component {
   constructor(props) {
     super(props);
+    this.state = { activeTab: 0 } 
   }
 
   render() {
     return(
-      <div>
-        <Tabs>
+      <div className="category-tabs">
+         <Tabs activeTab={this.state.activeTab} onChange={(tabId) => this.setState({ activeTab: tabId })} ripple>
           <Tab>Front End</Tab>
           <Tab>Full Stack</Tab>
         </Tabs>
