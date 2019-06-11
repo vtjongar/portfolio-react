735c86bd6971b4b48bb27c8db29bdecda7193692
Vanessa Henderson <bookvanny@gmail.com>
2019-06-10 21:55:37 -0400

    added toggle function and conditionals for work tabs

--- src/components/work.js
+++ src/components/work.js
@@ -1,22 +1,43 @@
-import React, { Component } from 'react';
-import { Tabs, Tab } from 'react-mdl';
-
+import React, { Component } from "react";
+import { Tabs, Tab } from "react-mdl";
 
 class Projects extends Component {
   constructor(props) {
     super(props);
-    this.state = { activeTab: 0 } 
+    this.state = { activeTab: 0 };
+  }
+
+  toggleCategories() {
+    if (this.state.activeTab === 0) {
+      return (
+        <div>
+          <h1>This is my front end work!</h1>
+        </div>
+      );
+    } else if (this.state.activeTab === 1) {
+      return (
+        <div>
+          <h1>This is my full stack work!</h1>
+        </div>
+      );
+    }
   }
 
   render() {
-    return(
+    return (
       <div className="category-tabs">
-         <Tabs activeTab={this.state.activeTab} onChange={(tabId) => this.setState({ activeTab: tabId })} ripple>
+        <Tabs
+          activeTab={this.state.activeTab}
+          onChange={tabId => this.setState({ activeTab: tabId })}
+          ripple
+        >
           <Tab>Front End</Tab>
           <Tab>Full Stack</Tab>
         </Tabs>
+
+        <section className="projects-grid">{this.toggleCategories()}</section>
       </div>
-    )
+    );
   }
 }
 export default Projects;
