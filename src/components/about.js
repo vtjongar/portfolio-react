005e49b62f968a6789fe375c3a61543205c19bf0
Vanessa Henderson <bookvanny@gmail.com>
2019-06-11 20:54:15 -0400

    styled all components to look more in sync

--- src/components/about.js
+++ src/components/about.js
@@ -12,13 +12,12 @@ class About extends Component {
         <div className="about-text">
        
 
-<p style={{ width: "75%", margin: "auto", paddingTop: "1em" }}>
-              "At vero eos et accusamus et iusto odio dignissimos ducimus qui
+<p className="about-paragraph" style={{ width: "75%", margin: "auto", paddingTop: "1em" }}>
+              At vero eos et accusamus et iusto odio dignissimos ducimus qui
               blanditiis praesentium voluptatum deleniti atque corrupti quos
               dolores et quas molestias excepturi sint occaecati cupiditate non
               provident, similique sunt in culpa qui officia deserunt mollitia
-              animi, id est laborum et dolorum fuga. Et harum quidem rerum
-              facilis est et expedita distinctio."
+              animi, id est laborum et dolorum fuga. 
             </p>
             </div>
       </div>
